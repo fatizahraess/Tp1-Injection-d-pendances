b40f8c97c8cafcadba3ea210143b9f542f1ffac7
fatima zahra <fatimazahraessadiki30@gmail.com>
2026-02-24 21:44:40 +0000

    Injection des dépendances par instanciation dynamique

--- JEE/src/main/java/net/essadiki/pres/Pres2.java
+++ JEE/src/main/java/net/essadiki/pres/Pres2.java
@@ -1,7 +1,23 @@
 package net.essadiki.pres;
 
+import net.essadiki.dao.IDao;
+import net.essadiki.metier.IMetier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
 public class Pres2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Scanner scanner=new Scanner(new File("config.txt"));
+    String daoClassName = scanner.nextLine();
+    Class cDao = Class.forName(daoClassName);
+   IDao d=(IDao) cDao.newInstance();
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier=(IMetier) cDao.getConstructor(IDao.class).newInstance(d);
 
     }
 }
